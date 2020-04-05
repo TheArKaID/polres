0d4b87f6b66e19d3e1949a5836af1dd38210d72d
Hafizh Pratama <hafizhpratama99.com>
2020-04-05 23:39:00 +0700

    tambahin foto dan perbagus slide show home

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -15,10 +15,10 @@ Polrestabes Yogyakarta
             <div class="container">
                 <div class="row">
                     <div class="col-xl-12">
-                        <div class="slider_text text-center">
+                        {{-- <div class="slider_text text-center">
                             <h3>Montana Resort</h3>
                             <p>Unlock to enjoy the view of Martine</p>
-                        </div>
+                        </div> --}}
                     </div>
                 </div>
             </div>
@@ -27,34 +27,10 @@ Polrestabes Yogyakarta
             <div class="container">
                 <div class="row">
                     <div class="col-xl-12">
-                        <div class="slider_text text-center">
+                        {{-- <div class="slider_text text-center">
                             <h3>Life is Beautiful</h3>
                             <p>Unlock to enjoy the view of Martine</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="single_slider d-flex align-items-center justify-content-center slider_bg_1">
-            <div class="container">
-                <div class="row">
-                    <div class="col-xl-12">
-                        <div class="slider_text text-center">
-                            <h3>Montana Resort</h3>
-                            <p>Unlock to enjoy the view of Martine</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="single_slider  d-flex align-items-center justify-content-center slider_bg_2">
-            <div class="container">
-                <div class="row">
-                    <div class="col-xl-12">
-                        <div class="slider_text text-center">
-                            <h3>Life is Beautiful</h3>
-                            <p>Unlock to enjoy the view of Martine</p>
-                        </div>
+                        </div> --}}
                     </div>
                 </div>
             </div>
