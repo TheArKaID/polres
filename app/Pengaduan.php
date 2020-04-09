6df7ad5de79d9dd2754fa9fef81ffd02db3a8c9a
Arifia Kasastra R <arka.progammer@gmail.com>
2020-04-10 02:30:41 +0700

    Create Table and Model Pengaduan

---
+++ app/Pengaduan.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Pengaduan extends Model
+{
+    protected $table = 'pengaduan';
+    
+    protected $fillable = [
+        'nama', 'email', 'notelpon', 'kategori', 'jeniskelamin', 'penjelasan', 'status'
+    ];
+}
---
+++ database/migrations/2020_04_09_192648_create_pengaduan_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePengaduanTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pengaduan', function (Blueprint $table) {
+            $table->id();
+            $table->string("nama");
+            $table->string("email");
+            $table->string("notelpon");
+            $table->string("kategori");
+            $table->string("jeniskelamin");
+            $table->text("penjelasan");
+            $table->string("status");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pengaduan');
+    }
+}
