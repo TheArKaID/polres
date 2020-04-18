d160417bb852dd9c9f52165d92627dfc7620fcc2
Arifia Kasastra R <arka.progammer@gmail.com>
2020-04-18 13:30:40 +0700

    Remove pagination in Pelayanan

--- resources/views/pages/pelayanan/pelayanan-all.blade.php
+++ resources/views/pages/pelayanan/pelayanan-all.blade.php
@@ -40,25 +40,4 @@ Polres Batang
     </div>
 </div>
 <!-- offers_area_end -->
-<nav class="blog-pagination justify-content-center d-flex mb-100 mt-0">
-    <ul class="pagination">
-        <li class="page-item">
-            <a href="#" class="page-link" aria-label="Previous">
-                <i class="ti-angle-left"></i>
-            </a>
-        </li>
-        <li class="page-item">
-            <a href="#" class="page-link">1</a>
-        </li>
-        <li class="page-item active">
-            <a href="#" class="page-link">2</a>
-        </li>
-        <li class="page-item">
-            <a href="#" class="page-link" aria-label="Next">
-                <i class="ti-angle-right"></i>
-            </a>
-        </li>
-    </ul>
-</nav>
-<!-- features_room_end -->
 @endsection
