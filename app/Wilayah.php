ac54ccb9432ec8f9bc5ed0e1ab019516a38b04c2
Arifia Kasastra R <arka.progammer@gmail.com>
2020-04-19 13:52:19 +0700

    Creating Wilayah Pengaduan Model

---
+++ app/Wilayah.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Wilayah extends Model
+{
+    protected $table = 'wilayah_polsek';
+    
+    protected $fillable = [
+        'wilayah'
+    ];
+}
