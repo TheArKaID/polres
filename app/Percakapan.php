7a792c7dc6192c90dd8dcd332d1a6cac66e84b22
Arifia Kasastra R <arka.progammer@gmail.com>
2020-04-25 13:43:01 +0700

    Buat Model dan Table Percakapan

---
+++ app/Percakapan.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Percakapan extends Model
+{
+    protected $table = 'percakapan';
+    
+    protected $fillable = [
+        'pengaduan_id', 'pesan'
+    ];
+}
