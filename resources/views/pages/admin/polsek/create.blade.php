78e849836b4cdd5f56c6020c320ea2ab982c46fc
Arifia Kasastra R <arka.progammer@gmail.com>
2020-04-30 02:27:08 +0700

    Tambahkan Keterangan Isian Penambahan Polsek Baru

--- resources/views/pages/admin/polsek/create.blade.php
+++ resources/views/pages/admin/polsek/create.blade.php
@@ -21,12 +21,14 @@
                 {{ csrf_field() }}
                 <div class="form-group">
                     <label for="namapolsek" class="bold">Nama Polsek</label>
-                    <input type="text" class="form-control" name="namapolsek" placeholder="Nama Polsek" value="{{old('namapolsek')}}" required>
+                    <input type="text" class="form-control" name="namapolsek" placeholder="Nama Kepolisian" value="{{old('namapolsek')}}" required>
+                    <p>*Masukkan Full Nama Kepolisian Sektor. Misal, <b>Kepolisian Sektor Indonesia Raya</b></p>
                 </div>
                 
                 <div class="form-group">
                     <label for="kapolsek" class="bold">Nama Kapolsek</label>
                     <input type="text" class="form-control" name="kapolsek" placeholder="Nama Kapolsek" value="{{old('kapolsek')}}" required>
+                    <p>*Masukkan Nama Lengkap dan Gelar dari Kapolsek. Misal, <i><b>Jenderal (Pol) Idham Azis</b></i> atau <i><b>Jenderal Pol. Drs. Idham Azis M.Si</b></i></p>
                 </div>
 
                 <div class="form-group">
@@ -42,12 +44,12 @@
 
                 <div class="form-group">
                     <label for="notelpon" class="bold">No. Telepon</label>
-                    <input type="text" class="form-control" name="notelpon" placeholder="notelpon" value="{{old('notelpon')}}">
+                    <input type="text" class="form-control" name="notelpon" placeholder="08XXXXXXXXXX" value="{{old('notelpon')}}">
                 </div>
 
                 <div class="form-group">
                     <label for="email" class="bold">Email</label>
-                    <input type="text" class="form-control" name="email" placeholder="email" value="{{old('email')}}">
+                    <input type="text" class="form-control" name="email" placeholder="polsek@polsek.id" value="{{old('email')}}">
                 </div>
                 
                 <div class="form-group">
@@ -68,16 +70,19 @@
                 <div class="form-group">
                     <label for="facebook" class="bold">User Facebook</label>
                     <input type="text" class="form-control" name="facebook" placeholder="User Facebook" value="{{old('facebook')}}">
+                    <p>Username Facebook berada setelah url Facebook. Misal, <i>https://facebook.com/<b>DivHumasPolri</b></i></p>
                 </div>
 
                 <div class="form-group">
                     <label for="twitter" class="bold">User Twitter</label>
                     <input type="text" class="form-control" name="twitter" placeholder="User Twitter" value="{{old('twitter')}}">
+                    <p>Username Twitter berada setelah url Twitter. Misal, <i>https://twitter.com/<b>DivHumas_Polri</b></i></p>
                 </div>
 
                 <div class="form-group">
                     <label for="instagram" class="bold">User Instagram</label>
                     <input type="text" class="form-control" name="instagram" placeholder="User Instagram" value="{{old('instagram')}}">
+                    <p>Username Instagram berada setelah url Instagram. Misal, <i>https://www.instagram.com/<b>divisihumaspolri</b></i></p>
                 </div>
 
                 <div class="form-group">
