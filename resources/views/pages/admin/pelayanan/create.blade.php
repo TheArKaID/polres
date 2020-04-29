aa854a241356a16871faca7443dd245ac5ab3a79
Arifia Kasastra R <arka.progammer@gmail.com>
2020-04-30 01:54:07 +0700

    Tampilkan Error dan Berhasil di Pelayanan Admin

--- resources/views/pages/admin/pelayanan/create.blade.php
+++ resources/views/pages/admin/pelayanan/create.blade.php
@@ -7,9 +7,14 @@
     <!-- Page Heading -->
     <div class="d-sm-flex align-items-center justify-content-between mb-4">
         <h1 class="h3 mb-0 text-gray-800">Tambah Pelayanan</h1>
-
     </div>
-
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </div>
+    @endif
     <div class="card shadow">
         <div class="card-body">
             <form action="/admin/pelayanan/prosestambah" method="POST" enctype="multipart/form-data">
