36de40e025e94bb84c98528f460ae8b99fb01301
Arifia Kasastra R <arka.progammer@gmail.com>
2020-04-30 02:04:20 +0700

    Tampilkan Pesan Error & Berhasil di Tupoksi Admin

--- resources/views/pages/admin/tupoksi/create.blade.php
+++ resources/views/pages/admin/tupoksi/create.blade.php
@@ -8,6 +8,13 @@
     <div class="d-sm-flex align-items-center justify-content-between mb-4">
         <h1 class="h3 mb-0 text-gray-800">Tambah Tupoksi</h1>
     </div>
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </div>
+    @endif
     <div class="card shadow">
         <div class="card-body">
             <form action="/admin/tupoksi/prosestambah" method="POST" enctype="multipart/form-data">
--- resources/views/pages/admin/tupoksi/edit.blade.php
+++ resources/views/pages/admin/tupoksi/edit.blade.php
@@ -8,6 +8,13 @@
     <div class="d-sm-flex align-items-center justify-content-between mb-4">
         <h1 class="h3 mb-0 text-gray-800">Edit Tupoksi</h1>
     </div>
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </div>
+    @endif
     <div class="card shadow">
         <div class="card-body">
             <form action="/admin/tupoksi/prosesedit" method="POST">
