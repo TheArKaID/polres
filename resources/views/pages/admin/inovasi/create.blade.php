40af8e1e13a61fc594d1ffdfc109a120c56a564c
Arifia Kasastra R <arka.progammer@gmail.com>
2020-04-30 01:51:45 +0700

    Tampilkan Pesan Error dan Berhasil Inovasi Admin

--- resources/views/pages/admin/inovasi/create.blade.php
+++ resources/views/pages/admin/inovasi/create.blade.php
@@ -3,7 +3,13 @@
 @section('content')
 <!-- Begin Page Content -->
 <div class="container-fluid">
-
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </div>
+    @endif
     <!-- Page Heading -->
     <div class="d-sm-flex align-items-center justify-content-between mb-4">
         <h1 class="h3 mb-0 text-gray-800">Tambah Inovasi</h1>
