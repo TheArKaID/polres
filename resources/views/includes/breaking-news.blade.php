22b6bd57c7a1bf9c47815adab048fb46fcf65c48
Arifia Kasastra R <arka.progammer@gmail.com>
2020-04-30 03:14:30 +0700

    Fix Tampilan Pengumuman

--- resources/views/includes/breaking-news.blade.php
+++ resources/views/includes/breaking-news.blade.php
@@ -1,12 +1,14 @@
-{{-- <div class="breaking-news pb-2 pt-2">
+<div class="breaking-news pb-2 pt-2">
     <div style="display:flex; color:white" class="ml-4 mr-4">
         <span aria-hidden="true"><i class="fas fa-bullhorn"></i></span>
-        <marquee behavior="scroll" direction="left">
-            | &nbsp; &nbsp; &nbsp;
-            @foreach ($pengumuman as $p)
-                {{$p->pengumuman}} &nbsp; &nbsp; &nbsp; | &nbsp; &nbsp; &nbsp;
-            @endforeach
-        </marquee>
+        @if ($pengumuman!=NULL)
+            <marquee behavior="scroll" direction="left">
+                | &nbsp; &nbsp; &nbsp;
+                @foreach ($pengumuman as $p)
+                    {{$p->pengumuman}} &nbsp; &nbsp; &nbsp; | &nbsp; &nbsp; &nbsp;
+                @endforeach
+            </marquee>
+        @endif
       </div>
 </div>
-     --}}
+    
