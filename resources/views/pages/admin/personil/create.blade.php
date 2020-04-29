fcca58b88bb4a8935ee1842ac26278dd8f717e7c
Arifia Kasastra R <arka.progammer@gmail.com>
2020-04-30 02:00:41 +0700

    Tampilkan Pesan Error & Berhasil di Personil Admin

--- resources/views/pages/admin/personil/create.blade.php
+++ resources/views/pages/admin/personil/create.blade.php
@@ -7,6 +7,13 @@
     <div class="d-sm-flex align-items-center justify-content-between mb-4">
         <h1 class="h3 mb-0 text-gray-800">Tambah Personil</h1>
     </div>
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </div>
+    @endif
     <div class="card shadow">
         <div class="card-body">
             <form action="/admin/personil/prosestambah" method="POST" enctype="multipart/form-data">
